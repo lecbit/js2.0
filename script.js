f7fb2b483defeffbf02836a085997910e8fbc255
unknown <artm-marych@rambler.ru>
2020-06-14 11:17:01 +0200

    add1

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Artem');
+document.getElementById('out').innerHTML='EDIT';
+document.querySelector('.out2').innerHTML='EDIT2';
+document.getElementById('out3').innerHTML='EDIT3';
+
+let a;
+a=document.getElementById('out4');
+a.innerHTML='EDIT555';
