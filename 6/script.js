c15460d2e1cbcae94cd77d2afc0915e5f5b16d2c
lecbit <artm-marych@rambler.ru>
2020-06-24 20:16:50 +0200

    add

--- 6/script.js
+++ 6/script.js
@@ -2,11 +2,20 @@ let out = document.querySelector('.out');
 
 console.log(out);
 
-for(let i = 0; i < 10; i++)
+/*for(let i = 0; i < 10; i++)
 {
   for(let j = 0; j < 10; j++)
   {
     out.innerHTML = out.innerHTML + "*" ;
   }
   out.innerHTML = out.innerHTML + '<br>';
+}*/
+
+for(let i = 1;i < 10;i++)
+{
+for(let j = 1;j < 10;j++)
+ {
+   out.innerHTML += `${i}*${j}=${i * j}<br>`;
+ }
+ out.innerHTML = out.innerHTML + '<hr>';
 }
