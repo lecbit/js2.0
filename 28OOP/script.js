a567a8942c076c3dabd618d745cf2c8f2752a2e0
lecbit <artm-marych@rambler.ru>
2020-07-13 11:01:56 +0200

    edit

--- 28OOP/script.js
+++ 28OOP/script.js
@@ -9,10 +9,10 @@ myAlert("hello", "orange", ".test2");
 
 
 
-let m = new Alert('My message', 'red', '.test');
+/*let m = new Alert('My message', 'red', '.test');
 console.log(m);
 m.showAlert();
-
+*/
 let m2 = new Alert2('My message', 'red', '.test', 'account_balance');
 m2.showIconAlert();
 m2.myAlert(this.message);
